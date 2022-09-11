c100ee920b67439e8d0ada1c225a0b95ca2abc63
Katresnass <112958949+Katresnass@users.noreply.github.com>
2022-09-11 21:34:14 +0700

    Add files via upload

---
+++ Nyobain C.cpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+int main(int argc, char * argv[])
+{
+	char nama[30];
+	cout<<"Insert Your Name : ";
+	cin>>nama;
+	cout<<endl;
+	cout<<"HAI "<<nama<<endl;
+	cout<<"Salam Kenal";
+	cout<<"Saya Imanuel";
+	cout<<"\n\n";
+	system("PAUSE");
+	return EXIT_SUCCESS;
+}
+	
+
